40eef4264a8ea4a3a427d83319ebbb5d118bd390
Nayeem An-noor <nayeeman49@googlemail.com>
2026-06-28 14:58:58 +0100

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/nayeem_films/",
+  base: "/nayeem_films/index.html",
 });
 
 
