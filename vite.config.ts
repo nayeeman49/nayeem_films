d48066587dd9ba19312b8ab670700d6c58778f00
Nayeem An-noor <nayeeman49@googlemail.com>
2026-06-28 14:49:34 +0100

    Add base path for deployment
    Set base path for deployment in Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/nayeem_films/",
+});
 
 
 function figmaAssetResolver() {
